ac451a10305ad14bd7658da9fd5aeade356498c7
Kirill Tsyganov <kirill.tsyganov@outlook.com.au>
2025-09-15 12:29:21 +1000

    uplifted forecast model class

--- model/forecast_models.py
+++ model/forecast_models.py
@@ -2,6 +2,7 @@
 
 import os
 import pickle
+import pandas as pd
 
 
 class ForecastModels:
@@ -24,11 +25,15 @@ class ForecastModels:
             "Education": 11,
         }
 
+        # self._baskets_rev = {v: k for k, v in self._baskets.items()}
+
         self.debug = debug
 
         self.artifacts_dir = artifacts_dir
         self.models = {}
         self._load_models()
+
+        self.forecast_results = None
 
     def __repr__(self):
         msg = "ForecastModels("
@@ -116,23 +121,99 @@ class ForecastModels:
 
         return model
 
-    def forecast(self, basket_idx, period=1):
+    def forecast(self, period=1, basket_idx=None):
         """
-        Forecast the next value(s) for a specific CPI basket using basket index
+        Forecast the next value(s) for all CPI baskets.
 
         Args:
-            basket_idx (int): Index of the CPI basket to forecast.
             period (int): Number of quarters to forecast. Default is 1, meaning the next quarter.
+            basket_idx (int, list): Index of the CPI basket to forecast. If None, forecasts all baskets.
 
         Returns:
-            pd.Series: Forecasted values for the specified number of quarters.
+            dict: A dictionary with basket indices as keys and their corresponding forecast results.
         """
 
         assert period >= 1, "Period must be at least 1"
         assert isinstance(period, int), "Period must be an integer"
 
-        # TODO: need to check if basket_idx is valid
-        model = self.get_model(basket_idx)
-        forecast = model.forecast(steps=period)
+        self.forecast_results = {}
 
-        return forecast
+        indices = list(self.models.keys())
+
+        if basket_idx is not None:
+            indices = [basket_idx]
+
+        print(f"DEBUG: Indices -> {indices}")
+
+        for index in indices:
+            model = self.get_model(index)
+            results = model.get_forecast(steps=period)
+
+            # TODO: overwrite previous results of a single forecasting.
+            # I want to do that, but want to change messaging better
+            if period not in self.forecast_results:
+                self.forecast_results[period] = {}
+            self.forecast_results[period][index] = results
+
+    def get_forecast_results(self):
+        """
+        Get the latest forecast results.
+
+        Returns:
+            PredictionResults: The latest forecasted values and confidence intervals.
+        """
+
+        if self.forecast_results is None:
+            raise ValueError(
+                "No forecast has been made yet. run models.forecast() or models.forecast_all()"
+            )
+
+        return self.forecast_results
+
+    def results(self, period=1, output_format="json"):
+        """
+        Prepare results for HTTP response
+
+        Args:
+            forecast_results (PredictionResults): The forecasted values and confidence intervals.
+            output_format (str): Output format, either 'json' or 'dataframe'. Default is 'json'.
+        Returns:
+            dict: A dictionary containing the forecast and confidence intervals.
+        """
+
+        results = []
+
+        for _, forecast_results in self.forecast_results[period].items():
+            forecast = float(forecast_results.predicted_mean)
+            # NOTE:
+            # A 95% confidence interval means that
+            # if you were to repeat this forecasting process 100 times,
+            # you would expect the true value to fall within this range about 95 times.
+            conf_int = forecast_results.conf_int()
+            conf_int = conf_int.reset_index()
+            # TODO: not very robust
+            basket_name = conf_int.columns[1].split("lower ")[1]
+
+            period = str(conf_int.iloc[0, 0])
+            lower = float(conf_int.iloc[0, 1])
+            upper = float(conf_int.iloc[0, 2])
+
+            results.append(
+                {
+                    "basket_name": basket_name,
+                    "period": str(period),
+                    "forecast": forecast,
+                    "conf_int": (lower, upper),
+                }
+            )
+
+        if output_format == "dataframe":
+            df = pd.DataFrame(results)
+            df[["conf_int_lower", "conf_int_upper"]] = pd.DataFrame(
+                df["conf_int"].tolist(), index=df.index
+            )
+            df = df.drop(columns=["conf_int"])
+
+            return df
+
+        return results
