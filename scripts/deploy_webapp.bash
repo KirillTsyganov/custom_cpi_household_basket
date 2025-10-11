a543796f805c3e27aeb6cb460e4e75655dc2a9b5
Kirill Tsyganov <kirill.tsyganov@outlook.com.au>
2025-10-11 21:06:02 +1100

    updated scripts

---
+++ scripts/deploy_webapp.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+deploy_dir="app"
+deploy_artifact="${deploy_dir}.zip"
+
+origin=$(dirname ${BASH_SOURCE[0]})
+echo "MSG: Origin script path: ${origin}"
+cd ${origin}/../${deploy_dir}
+
+# echo "" >> "requirements.txt"
+# echo "gunicorn==20.1.0" >> "requirements.txt"
+
+# echo "gunicorn --bind=0.0.0.0 --timeout 600 run:app" > "startup.txt"
+
+rg_name="cpiforecasting-rg"
+webapp_name="cpiforecasting-app-webapp"
+
+echo "MSG: Deployment stage"
+
+zip -r ${deploy_artifact} .
+
+az webapp \
+        deploy \
+            --resource-group ${rg_name} \
+            --name ${webapp_name} \
+            --src-path ${deploy_artifact} \
+            --type zip
+
+rm ${deploy_artifact}
