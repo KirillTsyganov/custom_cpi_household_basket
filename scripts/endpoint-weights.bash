54543cfe6f571b7142c9c71480832d8c03394847
Kirill Tsyganov <kirill.tsyganov@outlook.com.au>
2025-10-14 21:54:11 +1100

    updated endpoint testing scripts

---
+++ scripts/endpoint-weights.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function help() {
+  echo "USAGE: ./$(basename ${0}) <local|remote>"
+  echo ""
+  echo "  Options:"
+  echo ""
+  echo "    - local  - user localhost url"
+  echo "    - remote - user remote azure url"
+  echo ""
+}
+
+payload='{
+    "basket_idx": 0,
+    "period": 1
+}'
+
+if [[ ${1} == "local" ]]; then
+  url="http://localhost:7071/api/cpi_weights"
+elif [[ ${1} == "remote" ]]; then
+  url="https://cpiforecasting-app.azurewebsites.net/api/cpi_weights"
+else
+  help
+  exit 1
+fi
+
+curl -X GET \
+  ${url}
